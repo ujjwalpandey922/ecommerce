90b8394a491943d1127fa2670a6864d39197b0b3
Ujjwal Pandey <ujjwalpandey922@gmail.com>
2024-02-10 19:44:02 +0530

    feat:push not sign up users to login page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,8 +1,19 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Main from "../components/Main";
+import { auth } from "../config/firebase";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  // Hook for programmatic navigation
+  const router = useNavigate();
+  useEffect(() => {
+    if (!auth?.currentUser?.displayName || !auth?.currentUser?.email) {
+      router("/signup");
+    }
+  }, []);
   return (
     <div className="w-full min-h-screen flex  gap-2 bg-neutral-900">
       <div className="bg-neutral-900 flex justify-between max-w-[80rem] mx-auto px-2 flex-col w-full md:flex-1">
